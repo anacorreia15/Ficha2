616baeab5581849bbb5939984a77bfcae3ce037a
anacorreia_15 <ana.correia2@ipcbcampus.pt>
2020-11-04 16:13:34 +0000

    Initial commit

---
+++ app/src/main/java/pt/pdm/ficha2/MainActivity.java
@@ -0,0 +1,23 @@
+package pt.pdm.ficha2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+   /* BLOCO DE CÓDIGO DENTRO DO BUTÃO
+        Intent enviarInformacao = new Intent(this, MainActivity2.class);
+        startActivities(enviarInformacao);
+        enviarInformacao.putExtra("Valor1", "Conteudo1");
+        enviarInformacao.putExtra("Valor2", "Conteudo2");*/
+
+}
---
+++ app/src/main/java/pt/pdm/ficha2/MainActivity2.java
@@ -0,0 +1,20 @@
+package pt.pdm.ficha2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+    /*BLOCO DE CÓDIGO NO MAIN2
+    * Bundle extras = getIntent().getExtras();
+    * String valor1 = extras.getString("Valor1");
+    * String valor2 = extras.getString("Valor2");
+    * */
+}
